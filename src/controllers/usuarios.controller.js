cab44b255c4fe48344864ce744b3e2a26dd5e649
Rian Loretti <rian.souza0904@gmail.com>
2026-04-30 23:24:26 +0000

    Senac projeto

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -1,34 +1,34 @@
-const usuarios = [ 
+let usuarios = [ 
       {    
-    id: Date.now(),
-    nome: "Cleitin",
-    telefone: "(27) 4002-8922",
-    email: "cleitin@email.com",
-    idade: 67,
+    "id": Date.now(),
+    "nome": "Cleitin",
+    "telefone": "(27) 4002-8922",
+    "email": "cleitin@email.com",
+    "idade": 67,
   },
   {
-    id: Date.now(),
-    nome: "Rian",
-    telefone: "(27) 99999-1234",
-    email: "rian@email.com",
-    idade: 19
+    "id": Date.now(),
+    "nome": "Rian",
+    "telefone": "(27) 99999-1234",
+    "email": "rian@email.com",
+    "idade": 19
   },
   {
-    id: Date.now(),
-    nome: "Carlos",
-    telefone: "(27) 98888-5678",
-    email: "Carlos@email.com",
-    idade: 18
+    "id": Date.now(),
+    "nome": "Carlos",
+    "telefone": "(27) 98888-5678",
+    "email": "Carlos@email.com",
+    "idade": 18
   },
   {
-    id: Date.now(),
-    nome: "Nolan",
-    telefone: "(27) 97777-4321",
-    email: "nolan@email.com",
-    idade: 25
+    "id": Date.now(),
+    "nome": "Nolan",
+    "telefone": "(27) 97777-4321",
+    "email": "nolan@email.com",
+    "idade": 25
   }
 ]
 
-const listar = (req, res) => {res.json([])}
+const listar = (req, res) => {res.json(usuarios)}
 
 export default { listar }
