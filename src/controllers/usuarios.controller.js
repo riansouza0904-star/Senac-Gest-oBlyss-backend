c20e801a3381a6de1890427256af8f5f39960d0c
Rian Loretti <rian.souza0904@gmail.com>
2026-05-01 00:24:37 +0000

    Projeto Senac

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -1,27 +1,27 @@
 let usuarios = [ 
       {    
-    "id": Date.now(),
+    id: 1,
     "nome": "Cleitin",
     "telefone": "(27) 4002-8922",
     "email": "cleitin@email.com",
     "idade": 67,
   },
   {
-    "id": Date.now(),
+    id: 2,
     "nome": "Rian",
     "telefone": "(27) 99999-1234",
     "email": "rian@email.com",
     "idade": 19
   },
   {
-    "id": Date.now(),
+    id: 3,
     "nome": "Carlos",
     "telefone": "(27) 98888-5678",
     "email": "Carlos@email.com",
     "idade": 18
   },
   {
-    "id": Date.now(),
+    id: 4,
     "nome": "Nolan",
     "telefone": "(27) 97777-4321",
     "email": "nolan@email.com",
@@ -30,5 +30,9 @@ let usuarios = [
 ]
 
 const listar = (req, res) => {res.json(usuarios)}
-
-export default { listar }
+const filtrarid = (req, res) => {
+  const id = perseInt(req.params.id) 
+  const usuarios = usuarios.find(t => t.id === id)
+  res.json(usuarios)
+}
+export default { listar, filtrarid} 
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -4,5 +4,7 @@ import uc from '../controllers/usuarios.controller.js'
    const router = express.Router();
 
 router.get("/", uc.listar);
+router.get("/", uc.filtrarid);
+
 
 export default router;
